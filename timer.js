7e4638eac8ee9e5a6f3f7165b84c59b5b26b9ca1
gyanchandk <gyanchand1294@gmail.com>
2017-08-11 00:17:02 +0530

    changes made on timer.js

--- timer.js
+++ timer.js
@@ -1,4 +1,4 @@
-//var timer=document.getElementById("timer").innerHTML("hello world");
+
 
 window.onload = function(e){ 
 
@@ -8,39 +8,63 @@ window.onload = function(e){
 	 function myFunction() {
     
     document.getElementById("currentTime").innerHTML=moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-     
+                                                         
     //current timing
-    var startHour=parseInt(moment().format('h'));
+    var startHour=parseInt(moment().format('H'));    
     var startMinute=parseInt(moment().format('m'));
     var startSecond=parseInt(moment().format('s'));
     var startDay=parseInt(moment().format('D'));
     var startMonth=parseInt(moment().format('M'));
-    var startYear=parseInt(moment().format('Y'));  //current date and time
+    var startYear=parseInt(moment().format('Y'));  //current date and time ; 
+	//                                                         
+	
+	/* ---------------------------------------------------------------------
+	  by default start time the current time and date.
+	  if you want to change the start time to know to time-gap between two dates like "1 FEB 2017,01:10:15pm"
+	  just add // in the beginning of line c and line d,and make respective changes you need;
+	  and you can reset to default by removing // you have added;
+		  you can make similar changes on end time by changing 
+		  between line e and line f;
+      	  
+	*/
+	//------------------------------------------------------------------------
+	
+	
+	/*                                               // line c
+	 
+	var  startHour=1;
+    var startMinute=10;
+    var startSecond=25;
+    var startDay=1;
+    var startMonth=2;
+    var startYear=2017;
+	*/                                                //line d
     
     //end timing
-    var endHour=9;
+    var endHour=9;                                  //line e
     var endMinute=0;
     var endSecond=0;
     var endDay=26;  
     var endMonth=11;
-    var endYear=2017;     //09:00:00 26th november 2017
+    var endYear=2017;     //09:00:00 26th november 2017  //line f
 
 
     //find the difference
-    var start=moment([startYear,startMonth,startDay,startHour,startMinute,startSecond]);
-    var end = moment([endYear,endMonth,endDay,endHour,endMinute,endSecond]);
+    var start=moment([startYear,startMonth-1,startDay,startHour,startMinute,startSecond]);
+    var end = moment([endYear,endMonth-1,endDay,endHour,endMinute,endSecond]);
 
    
     //calculation
     var remainingSecond=parseInt(end.diff(start,"seconds"));
     
-    var hours = Math.round(remainingSecond/3600);
+    var hours = Math.floor(remainingSecond/3600);
     remainingSecond %=3600;
+	
 
-    var days = Math.round(hours/24);
+    var days = Math.floor(hours/24);
     hours%=24;
 
-    var minutes = Math.round(remainingSecond/60);
+    var minutes = Math.floor(remainingSecond/60);
     remainingSecond%=60;
 
     var seconds =remainingSecond;
@@ -53,13 +77,13 @@ window.onload = function(e){
     //set the values to DOM
     var remainingTime = days+" d"+hours + " h"+ minutes + " m"+seconds+" s";
    
-    if(days>0)
+ 
       document.getElementById("days").innerHTML=days;
     
-    if(hours>0)
+
       document.getElementById("hours").innerHTML=hours;
     
-    if(minutes>0)
+   
       document.getElementById("minutes").innerHTML=minutes;
 
     document.getElementById("seconds").innerHTML=seconds;
